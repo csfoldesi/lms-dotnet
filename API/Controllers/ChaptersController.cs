0a6ba4a5fe94c712125962b04ff67364139c2b7c
Csaba Földesi <csabus@gmail.com>
2025-03-09 15:22:15 +0100

    Get Chapter API

---
+++ API/Controllers/ChaptersController.cs
@@ -0,0 +1,14 @@
+﻿using Application.Chapters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ChaptersController : BaseApiController
+{
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> Get(Guid Id)
+    {
+        var result = await Mediator.Send(new Get.Query { Id = Id });
+        return HandleResult(result);
+    }
+}
---
+++ Application/Chapters/Get.cs
@@ -0,0 +1,45 @@
+﻿using Application.Common;
+using Application.Common.Interfaces;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Chapters;
+
+public class Get
+{
+    public class Query : IRequest<Result<ChapterDto>>
+    {
+        public required Guid Id { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Query, Result<ChapterDto>>
+    {
+        private readonly IDataContext _dataContext;
+        private readonly IMapper _mapper;
+
+        public Handler(IDataContext dataContext, IMapper mapper)
+        {
+            _dataContext = dataContext;
+            _mapper = mapper;
+        }
+
+        public async Task<Result<ChapterDto>> Handle(
+            Query request,
+            CancellationToken cancellationToken
+        )
+        {
+            var chapter = await _dataContext.Chapters.SingleOrDefaultAsync(
+                chapter => chapter.Id == request.Id,
+                cancellationToken: cancellationToken
+            );
+
+            if (chapter == null)
+            {
+                return Result<ChapterDto>.NotFound();
+            }
+
+            return Result<ChapterDto>.Success(_mapper.Map<ChapterDto>(chapter));
+        }
+    }
+}
