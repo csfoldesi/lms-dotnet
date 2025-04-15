ceb528625522a668f7bd36c17473e04e697abd98
Csaba Földesi <csabus@gmail.com>
2025-04-15 10:36:03 +0200

    Logger updates

--- API/Middleware/ErrorHandlingMiddleware.cs
+++ API/Middleware/ErrorHandlingMiddleware.cs
@@ -1,15 +1,19 @@
 ﻿using System.Net;
 using System.Text.Json;
+using Application.Common.Interfaces;
+using NLog;
 
 namespace API.Middleware;
 
 public class ErrorHandlingMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly ILoggerService _logger;
 
-    public ErrorHandlingMiddleware(RequestDelegate next)
+    public ErrorHandlingMiddleware(RequestDelegate next, ILoggerService logger)
     {
         _next = next;
+        _logger = logger;
     }
 
     public async Task Invoke(HttpContext context)
@@ -20,34 +24,42 @@ public class ErrorHandlingMiddleware
         }
         catch (Exception ex)
         {
-            await HandleExceptionAsync(context, ex);
+            await HandleExceptionAsync(context, ex, _logger);
         }
     }
 
-    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    private static Task HandleExceptionAsync(
+        HttpContext context,
+        Exception exception,
+        ILoggerService logger
+    )
     {
         var code = HttpStatusCode.InternalServerError;
         var result = string.Empty;
 
-        switch (exception)
+        using (ScopeContext.PushProperty("context", "TestContext"))
         {
-            case Application.Common.Exceptions.NotFoundException _:
-                code = HttpStatusCode.NotFound;
-                result = JsonSerializer.Serialize(
-                    new { IsSuccess = false, ErrorMessage = exception.Message }
-                );
-                break;
-            case Application.Common.Exceptions.UnauthorizedException _:
-                code = HttpStatusCode.Unauthorized;
-                result = JsonSerializer.Serialize(
-                    new { IsSuccess = false, ErrorMessage = exception.Message }
-                );
-                break;
-            default:
-                result = JsonSerializer.Serialize(
-                    new { error = "An error occurred while processing your request." }
-                );
-                break;
+            switch (exception)
+            {
+                case Application.Common.Exceptions.NotFoundException _:
+                    code = HttpStatusCode.NotFound;
+                    result = JsonSerializer.Serialize(
+                        new { IsSuccess = false, ErrorMessage = exception.Message }
+                    );
+                    break;
+                case Application.Common.Exceptions.UnauthorizedException _:
+                    code = HttpStatusCode.Unauthorized;
+                    result = JsonSerializer.Serialize(
+                        new { IsSuccess = false, ErrorMessage = exception.Message }
+                    );
+                    break;
+                default:
+                    result = JsonSerializer.Serialize(
+                        new { error = "An error occurred while processing your request." }
+                    );
+                    logger.LogError(exception, "An error occurred while processing your request.");
+                    break;
+            }
         }
 
         context.Response.ContentType = "application/json";
