d65aceb595de496fa9b7e6cd3b7fad46b2fcb992
Csaba Földesi <csabus@gmail.com>
2025-04-30 18:56:21 +0200

    Playback sidebar created

--- angular-client-app/src/app/playback/ui/playback-sidebar.component.ts
+++ angular-client-app/src/app/playback/ui/playback-sidebar.component.ts
@@ -1,14 +1,39 @@
 import { Component, input } from '@angular/core';
+import { Chapter } from '../../shared/types';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'lms-playback-sidebar',
-  imports: [],
+  imports: [MatListModule, MatIconModule, RouterModule, NgClass],
   template: `
-    <p>playback-sidebar works!</p>
-    <p>courseId: {{ courseId() }}</p>
+    <mat-list role="list">
+      @for(chapter of chapters(); track chapter.id) {
+      <a routerLink="/courses/{{ courseId() }}/chapters/{{ chapter.id }}">
+        <mat-list-item
+          role="listitem"
+          class="m-2"
+          [ngClass]="{
+            'border-blue-800 border-r-4': chapter.id === selectedChapterId()
+          }"
+        >
+          <div class="cursor-pointer flex">
+            <mat-icon matListItemIcon class="mr-2">
+              play_circle_outline
+            </mat-icon>
+            {{ chapter.title }}
+          </div>
+        </mat-list-item>
+      </a>
+      }
+    </mat-list>
   `,
   styles: ``,
 })
 export class PlaybackSidebarComponent {
-  courseId = input<string | null | undefined>();
+  chapters = input<Chapter[] | null>();
+  courseId = input.required<string>();
+  selectedChapterId = input.required<string>();
 }
